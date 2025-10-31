22c95e203fb1fe191e45da7fe6947a5ded59b31f
Hadiid Andri Yulison <hadiidandri2000@gmail.com>
2025-10-31 14:17:28 +0700

    LocaleContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
-import { Link, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/page";
 import NewNotePage from "./pages/notes/new/page";
 import NoteDetailPage from "./pages/notes/id/page";
@@ -10,11 +10,37 @@ import RegisterPage from "./pages/auth/register/page";
 import { ToastContainer } from "react-toastify";
 import ThemeContext from "./contexts/themeContext";
 import { authService } from "./services/auth.service";
+import Navigation from "./components/navigation";
+import LocaleContext from "./contexts/localeContext";
 
 function App() {
   const [authedUser, setAuthedUser] = useState(null);
   const [initializing, setInitializing] = useState(true);
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
+  const [locale, setLocale] = useState(localStorage.getItem("locale") || "id");
+
+  const toggleLocale = () => {
+    setLocale((prevLocale) => {
+      const newLocale = prevLocale === "id" ? "en" : "id";
+      localStorage.setItem("locale", newLocale);
+      return newLocale;
+    });
+  };
+
+  const selectLanguage = ({ id, en }) => {
+    if (id === undefined || en === undefined) {
+      return "language options is empty";
+    }
+    return locale === "id" ? id : en;
+  };
+
+  const localeContextValue = useMemo(() => {
+    return {
+      locale,
+      toggleLocale,
+      selectLanguage,
+    };
+  }, [locale]);
 
   const toggleTheme = () => {
     setTheme((prevTheme) => {
@@ -47,6 +73,10 @@ function App() {
     const { data } = await authService.getUserLogged();
     setAuthedUser(data);
   };
+  const onLogout = () => {
+    setAuthedUser(null);
+    authService.putAccessToken("");
+  };
 
   if (initializing) {
     return null;
@@ -55,173 +85,87 @@ function App() {
   if (!authedUser) {
     return (
       <>
-        <ToastContainer
-          position="top-center"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick={false}
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme={theme}
-        />{" "}
-        <ThemeContext.Provider value={themeContextValue}>
-          <div className="app-container">
-            <header>
-              <h1>
-                {" "}
-                <Link to="/">Aplikasi Catatan</Link>
-              </h1>
-              <div className="navigation">
-                <ul>
-                  {/* <li>
-                    <Link to="/archives">Arsip</Link>
-                  </li> */}
-                </ul>
-              </div>
+        <LocaleContext.Provider value={localeContextValue}>
+          <ThemeContext.Provider value={themeContextValue}>
+            <ToastContainer
+              position="top-center"
+              autoClose={3000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick={false}
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme={theme}
+            />{" "}
+            <div className="app-container">
+              <header>
+                <Navigation />
+              </header>
 
-              {/* Toggle theme button */}
-              <button
-                className="toggle-theme"
-                type="button"
-                onClick={toggleTheme}
-                aria-label="Toggle theme"
-                title="Toggle theme"
-              >
-                {theme === "light" ? (
-                  <svg
-                    stroke="currentColor"
-                    fill="currentColor"
-                    strokeWidth="0"
-                    viewBox="0 0 24 24"
-                    height="1em"
-                    width="1em"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path fill="none" d="M0 0h24v24H0z"></path>
-                    <path d="M12 9c1.65 0 3 1.35 3 3s-1.35 3-3 3-3-1.35-3-3 1.35-3 3-3m0-2c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5zM2 13h2c.55 0 1-.45 1-1s-.45-1-1-1H2c-.55 0-1 .45-1 1s.45 1 1 1zm18 0h2c.55 0 1-.45 1-1s-.45-1-1-1h-2c-.55 0-1 .45-1 1s.45 1 1 1zM11 2v2c0 .55.45 1 1 1s1-.45 1-1V2c0-.55-.45-1-1-1s-1 .45-1 1zm0 18v2c0 .55.45 1 1 1s1-.45 1-1v-2c0-.55-.45-1-1-1s-1 .45-1 1zM5.99 4.58a.996.996 0 00-1.41 0 .996.996 0 000 1.41l1.06 1.06c.39.39 1.03.39 1.41 0s.39-1.03 0-1.41L5.99 4.58zm12.37 12.37a.996.996 0 00-1.41 0 .996.996 0 000 1.41l1.06 1.06c.39.39 1.03.39 1.41 0a.996.996 0 000-1.41l-1.06-1.06zm1.06-10.96a.996.996 0 000-1.41.996.996 0 00-1.41 0l-1.06 1.06c-.39.39-.39 1.03 0 1.41s1.03.39 1.41 0l1.06-1.06zM7.05 18.36a.996.996 0 000-1.41.996.996 0 00-1.41 0l-1.06 1.06c-.39.39-.39 1.03 0 1.41s1.03.39 1.41 0l1.06-1.06z"></path>
-                  </svg>
-                ) : (
-                  <svg
-                    stroke="currentColor"
-                    fill="currentColor"
-                    strokeWidth="0"
-                    viewBox="0 0 24 24"
-                    height="1em"
-                    width="1em"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path fill="none" d="M0 0h24v24H0z"></path>
-                    <path d="M9.37 5.51A7.35 7.35 0 009.1 7.5c0 4.08 3.32 7.4 7.4 7.4.68 0 1.35-.09 1.99-.27A7.014 7.014 0 0112 19c-3.86 0-7-3.14-7-7 0-2.93 1.81-5.45 4.37-6.49zM12 3a9 9 0 109 9c0-.46-.04-.92-.1-1.36a5.389 5.389 0 01-4.4 2.26 5.403 5.403 0 01-3.14-9.8c-.44-.06-.9-.1-1.36-.1z"></path>
-                  </svg>
-                )}
-              </button>
-            </header>
-
-            <main>
-              <Routes>
-                <Route
-                  path="/*"
-                  element={<LoginPage loginSuccess={onLoginSuccess} />}
-                />
-                <Route
-                  path="/register"
-                  element={<RegisterPage theme={theme} />}
-                />
-              </Routes>
-            </main>
-          </div>
-        </ThemeContext.Provider>
+              <main>
+                <Routes>
+                  <Route
+                    path="/*"
+                    element={<LoginPage loginSuccess={onLoginSuccess} />}
+                  />
+                  <Route
+                    path="/register"
+                    element={<RegisterPage theme={theme} />}
+                  />
+                </Routes>
+              </main>
+            </div>
+          </ThemeContext.Provider>
+        </LocaleContext.Provider>
       </>
     );
   }
   return (
     <>
-      <ToastContainer
-        position="top-center"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={false}
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme={theme}
-      />{" "}
-      <ThemeContext.Provider value={themeContextValue}>
-        <div className="app-container">
-          <header>
-            <h1>
-              {" "}
-              <Link to="/">Aplikasi Catatan</Link>
-            </h1>
-            <div className="navigation">
-              <ul>
-                <li>
-                  <Link to="/archives">Arsip</Link>
-                </li>
-              </ul>
-            </div>
-
-            {/* Toggle theme button */}
-            <button
-              className="toggle-theme"
-              type="button"
-              onClick={toggleTheme}
-              aria-label="Toggle theme"
-              title="Toggle theme"
-            >
-              {theme === "light" ? (
-                <svg
-                  stroke="currentColor"
-                  fill="currentColor"
-                  strokeWidth="0"
-                  viewBox="0 0 24 24"
-                  height="1em"
-                  width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path fill="none" d="M0 0h24v24H0z"></path>
-                  <path d="M12 9c1.65 0 3 1.35 3 3s-1.35 3-3 3-3-1.35-3-3 1.35-3 3-3m0-2c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5zM2 13h2c.55 0 1-.45 1-1s-.45-1-1-1H2c-.55 0-1 .45-1 1s.45 1 1 1zm18 0h2c.55 0 1-.45 1-1s-.45-1-1-1h-2c-.55 0-1 .45-1 1s.45 1 1 1zM11 2v2c0 .55.45 1 1 1s1-.45 1-1V2c0-.55-.45-1-1-1s-1 .45-1 1zm0 18v2c0 .55.45 1 1 1s1-.45 1-1v-2c0-.55-.45-1-1-1s-1 .45-1 1zM5.99 4.58a.996.996 0 00-1.41 0 .996.996 0 000 1.41l1.06 1.06c.39.39 1.03.39 1.41 0s.39-1.03 0-1.41L5.99 4.58zm12.37 12.37a.996.996 0 00-1.41 0 .996.996 0 000 1.41l1.06 1.06c.39.39 1.03.39 1.41 0a.996.996 0 000-1.41l-1.06-1.06zm1.06-10.96a.996.996 0 000-1.41.996.996 0 00-1.41 0l-1.06 1.06c-.39.39-.39 1.03 0 1.41s1.03.39 1.41 0l1.06-1.06zM7.05 18.36a.996.996 0 000-1.41.996.996 0 00-1.41 0l-1.06 1.06c-.39.39-.39 1.03 0 1.41s1.03.39 1.41 0l1.06-1.06z"></path>
-                </svg>
-              ) : (
-                <svg
-                  stroke="currentColor"
-                  fill="currentColor"
-                  strokeWidth="0"
-                  viewBox="0 0 24 24"
-                  height="1em"
-                  width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path fill="none" d="M0 0h24v24H0z"></path>
-                  <path d="M9.37 5.51A7.35 7.35 0 009.1 7.5c0 4.08 3.32 7.4 7.4 7.4.68 0 1.35-.09 1.99-.27A7.014 7.014 0 0112 19c-3.86 0-7-3.14-7-7 0-2.93 1.81-5.45 4.37-6.49zM12 3a9 9 0 109 9c0-.46-.04-.92-.1-1.36a5.389 5.389 0 01-4.4 2.26 5.403 5.403 0 01-3.14-9.8c-.44-.06-.9-.1-1.36-.1z"></path>
-                </svg>
-              )}
-            </button>
-          </header>
-
-          <main>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-
-              <Route
-                path="/notes/new"
-                element={<NewNotePage theme={theme} />}
+      <LocaleContext.Provider value={localeContextValue}>
+        <ThemeContext.Provider value={themeContextValue}>
+          <ToastContainer
+            position="top-center"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick={false}
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme={theme}
+          />{" "}
+          <div className="app-container">
+            <header>
+              <Navigation
+                logout={onLogout}
+                name={authedUser.name}
+                toggleTheme={toggleTheme}
               />
-              <Route
-                path="/notes/:id"
-                element={<NoteDetailPage theme={theme} />}
-              />
-              <Route path="/archives" element={<ArchivesPage />} />
-              <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-          </main>
-        </div>
-      </ThemeContext.Provider>
+            </header>
+
+            <main>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+
+                <Route
+                  path="/notes/new"
+                  element={<NewNotePage theme={theme} />}
+                />
+                <Route
+                  path="/notes/:id"
+                  element={<NoteDetailPage theme={theme} />}
+                />
+                <Route path="/archives" element={<ArchivesPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+              </Routes>
+            </main>
+          </div>
+        </ThemeContext.Provider>
+      </LocaleContext.Provider>
     </>
   );
 }
---
+++ src/components/navigation.jsx
@@ -0,0 +1,46 @@
+import { useContext } from "react";
+import ThemeContext from "../contexts/themeContext";
+import LocaleContext from "../contexts/localeContext";
+import { FiLogOut, FiMoon, FiSun } from "react-icons/fi";
+import { SiGoogletranslate } from "react-icons/si";
+import { Link } from "react-router-dom";
+
+export default function Navigation({ logout, name }) {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  const { toggleLocale, selectLanguage } = useContext(LocaleContext);
+
+  return (
+    <>
+      <h1>
+        {" "}
+        <Link to="/">
+          {selectLanguage({ id: "Aplikasi Catatan", en: "Notes App" })}
+        </Link>
+      </h1>
+      {logout !== undefined && (
+        <nav className="navigation">
+          <ul>
+            <li>
+              <Link to="/archives">
+                {selectLanguage({ id: "Arsip", en: "Archived" })}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+
+      {/* Toggle theme button */}
+      <button className="toggle-locale" onClick={toggleLocale}>
+        <SiGoogletranslate />
+      </button>
+      <button className="toggle-theme" onClick={toggleTheme}>
+        {theme === "light" ? <FiMoon /> : <FiSun />}
+      </button>
+      {logout !== undefined && (
+        <button className="button-logout" onClick={logout} title="Logout">
+          <FiLogOut /> {name}
+        </button>
+      )}
+    </>
+  );
+}
