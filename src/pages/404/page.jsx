8dc44c3208e63b42bcdd349e51584c8df34b1433
Hadiid Andri Yulison <hadiidandri2000@gmail.com>
2025-10-31 22:27:42 +0700

    selectLanguage 404

--- src/pages/404/page.jsx
+++ src/pages/404/page.jsx
@@ -1,8 +1,18 @@
+import { useContext } from "react";
+import LocaleContext from "../../contexts/localeContext";
+
 export default function NotFoundPage() {
+  const { selectLanguage } = useContext(LocaleContext);
+
   return (
     <>
       <h1>404</h1>
-      <p>Halaman tidak ditemukan</p>
+      <p>
+        {selectLanguage({
+          id: "Halaman tidak ditemukan",
+          en: "Page not found",
+        })}
+      </p>
     </>
   );
 }
